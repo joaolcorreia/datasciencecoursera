3ffb6689f250a08aae39930963930cf81e8f23d5
Joao Correia <joao.correia@gmail.com>
2014-06-10 23:31:26 -0700

    Program Assignment 1 - Part 3
    Part 3

--- R Programming/Programming Assignment 1/complete.R
+++ R Programming/Programming Assignment 1/complete.R
@@ -2,22 +2,19 @@
 # in each data file. 
 
 complete <- function(directory, id=1) {
-  
-  # Initialize the data.frame
-  df<-data.frame(id = numeric(), nobs = numeric())
-  
-  # Read the monitor files from the specified directory
-  for (i in id) {
-    id_fs<-formatC(i,width=3, flag="0")   
-    tmp<-read.csv(paste(directory,"/",as.character(id_fs),".csv", sep=''))
-    nrows <- nrow(tmp[complete.cases(tmp),])    
-    t<-data.frame('id'=i,'nobs'=nrows)
-    
-    df<-rbind(df,t)  
-    
-  }  
-  
-return(df)
-  
-# End
+        
+        # Initialize the data.frame
+        df<-data.frame(id = numeric(), nobs = numeric())
+        
+                # Read the monitor files from the specified directory
+                for (i in id) {
+                        id_fs<-formatC(i,width=3, flag="0")   
+                        tmp<-read.csv(paste(directory,"/",as.character(id_fs),".csv", sep=''))
+                        nrows <- nrow(tmp[complete.cases(tmp),])    
+                        t<-data.frame('id'=i,'nobs'=nrows)
+                        df<-rbind(df,t)                  
+                }  
+                
+        # End
+        return(df)
 }
--- R Programming/Programming Assignment 1/pollutantmean.R
+++ R Programming/Programming Assignment 1/pollutantmean.R
@@ -1,22 +1,21 @@
 # pollutantmean calculates the mean of a pollutant (sulfate or nitrate) 
 # across a specified list of monitors
 
-pollutantmean <- function(directory, pollutant, id=1:2){
+pollutantmean <- function(directory, pollutant, id=1:332){
+        
+        # Initialize the data frame
+        df<-data.frame(date = character(), sulfate = numeric(), nitrate = numeric(), ID = numeric())
+        
+                # Read the monitor files from the specified directory. Hack! Leading zeros
+                for (i in id) {
+                        id_fs<-formatC(i,width=3, flag="0")   
+                        tmp<-read.csv(paste(directory,"/",as.character(id_fs),".csv", sep=''))
+                        df<-rbind(df,tmp)      
+                }  
+        
+        # Calculate the mean of the pollutant
+        res<-mean(df[,pollutant],na.rm=TRUE)
 
-# Initialize the data frame
-df<-data.frame(date = character(), sulfate = numeric(), nitrate = numeric(), ID = numeric())
-
-  # Read the monitor files from the specified directory. Hack! Leading zeros
-  for (i in id) {
-    id_fs<-formatC(i,width=3, flag="0")   
-    tmp<-read.csv(paste(directory,"/",as.character(id_fs),".csv", sep=''))
-    df<-rbind(df,tmp)      
-  }  
-  
-# Calculate the mean of the pollutant
-res<-round(mean(df[,pollutant],na.rm=TRUE),3)
-
-return(res)
-
-# End
+        # End
+        return(res)        
 }
